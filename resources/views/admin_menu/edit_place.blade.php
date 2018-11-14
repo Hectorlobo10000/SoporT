71e0eb312a9649be2999c9af375282d754ec9eb7
SelvinM <sel2230@gmail.com>
2018-11-14 10:38:43 -0600

    Update resources/views/admin_menu/edit_place.blade.php

--- resources/views/admin_menu/edit_place.blade.php
+++ resources/views/admin_menu/edit_place.blade.php
@@ -7,8 +7,15 @@
   <form method="post" action="{{action('PlaceController@update',$lugare->id)}}">
 	@csrf
 	@method('PUT')
-	<label>Nombre:</label>
-	<input type="text" name="name" value="{{$lugare->name}}">
+	<label>Departamento:</label>
+	<input type="text" name="domain" value="{{$lugare->domain}}" class="formulario">
+
+	<label for="municipality">Municipio:</label>
+	<input type="text" name="municipality" value="{{$lugare->municipality}}" class="formulario">
+
+	<label>Dirección:</label>
+	<input type="text" name="address" value="{{$lugare->address}}" class="formulario">
+
     <button type="submit" class="btn btn-info">Modificar</button>
    </form>
 
