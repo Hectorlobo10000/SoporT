6682124fbcd2828bd13e0872d072e2a046f4febf
SelvinM <sel2230@gmail.com>
2018-11-16 15:50:25 -0600

    tecnico vistas

--- resources/views/technician_menu/finished.blade.php
+++ resources/views/technician_menu/finished.blade.php
@@ -6,13 +6,12 @@
 	<li><a href="{{route('pending')}}">Tareas pendientes</a></li>
 	<li><a href="{{route('initiated')}}">Tareas iniciadas</a></li>
 	<li class="active"><a href="{{route('finished')}}">Tareas finalizadas</a></li>
-	<li id="out"><a href="{{ url('/logout') }}">{{Auth::User()->name}} Cerrar Sesion</a></li>
 @endsection
 @section('records')
 	@foreach($tasks as $task)
 		@if($task->task_state_id===3)
 			@section('extra fields')
-				<th>ANOTACIONES</th>
+				<th></th>
 				<th><-----</th>
 			@endsection
 			<tr>
@@ -24,12 +23,13 @@
 				<td>{{$task->client->place->municipality}}</td>
 				<td>{{$task->client->place->address}}</td>
 				<td>{{$task->created_at}}</td>
+				<td><a href="">imprimir ticket</a></td>
 				<td>
 					<form action="{{route('update task state',['task'=>$task])}}" method="POST" id="form {{$task->id}}">
 						{{method_field('PATCH')}}
 	    				{{ csrf_field() }}
 						<input type="hidden" name="task_state_id" value="2">
-						<a class="btn btn-success"  href="javascript:{}" onclick="document.getElementById('form {{$task->id}}').submit(); return false;">Mover a iniciadas</a>
+						<a  href="javascript:{}" onclick="document.getElementById('form {{$task->id}}').submit(); return false;">mover a iniciadas</a>
 					</form>
 				</td>
 			</tr>
--- resources/views/technician_menu/initiated.blade.php
+++ resources/views/technician_menu/initiated.blade.php
@@ -6,13 +6,12 @@
 	<li><a href="{{route('pending')}}">Tareas pendientes</a></li>
 	<li class="active"><a href="{{route('initiated')}}">Tareas iniciadas</a></li>
 	<li><a href="{{route('finished')}}">Tareas finalizadas</a></li>
-	<li id="out"><a href="{{ url('/logout') }}">{{Auth::User()->name}} Cerrar Sesion</a></li>
 @endsection
 @section('records')
 	@foreach($tasks as $task)
 		@if($task->task_state_id===2)
 		@section('extra fields')
-			<th>ANOTACIONES</th>
+			<th></th>
 			<th><-----</th>
 			<th>-----></th>
 		@endsection
@@ -25,13 +24,13 @@
 				<td>{{$task->client->place->municipality}}</td>
 				<td>{{$task->client->place->address}}</td>
 				<td>{{$task->created_at}}</td>
-				<td><a class="btn btn-info" href="{{route('show task annotation',['task'=>$task])}}">Mostrar</a></td>
+				<td><a href="{{route('show task annotation',['task'=>$task])}}">mostrar anotación</a></td>
 				<td>
 					<form action="{{route('update task state',['task'=>$task])}}" method="POST" id="form 1 {{$task->id}}">
 						{{method_field('PATCH')}}
 	    				{{ csrf_field() }}
 						<input type="hidden" name="task_state_id" value="1">
-						<a class="btn btn-success" href="javascript:{}" onclick="document.getElementById('form 1 {{$task->id}}').submit(); return false;">Mover a pendientes</a>
+						<a href="javascript:{}" onclick="document.getElementById('form 1 {{$task->id}}').submit(); return false;">mover a pendientes</a>
 					</form>
 				</td>
 				<td>
@@ -39,7 +38,7 @@
 						{{method_field('PATCH')}}
 	    				{{ csrf_field() }}
 						<input type="hidden" name="task_state_id" value="3">
-						<a class="btn btn-success" href="javascript:{}" onclick="document.getElementById('form 2 {{$task->id}}').submit(); return false;">Mover a finalizadas</a>
+						<a href="javascript:{}" onclick="document.getElementById('form 2 {{$task->id}}').submit(); return false;">mover a finalizadas</a>
 					</form>
 				</td>
 			</tr>
