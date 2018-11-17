6c5071ad49267c2b83e854373c45b5e012348855
SelvinM <sel2230@gmail.com>
2018-11-17 13:26:32 -0600

    seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,5 +19,9 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'phone' => $faker->phoneNumber,
+        'role_id' => $faker->numberBetween($min = 2, $max = 3),
+        'department_id' => $faker->numberBetween($min = 1, $max = 10),
+        'place_id' => $faker->numberBetween($min = 1, $max = 50),
     ];
 });
